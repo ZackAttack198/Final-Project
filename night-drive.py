05de5512dee1acd10fdbfefb9ba139544eff2315
Zack Wilson <f.zero.gx@outlook.com>
2025-11-28 15:33:39 -0600

    Changed "road" class to infinitely scroll a series of road sprites that covers entire screen. Removed Image module. Added car sprite to road as simple blitted image. Removed "car" class.

--- night-drive.py
+++ night-drive.py
@@ -1,20 +1,21 @@
 import pygame
 import glob
 import os
-from PIL import Image
 import math
 import random
 
 
 class Road():
 
-    def __init__(self, y, tile_width=500, height=220,
+    def __init__(self, y, tile_width=500, height=220, speed=10,
                  road_color=(60, 60, 60), dash_color=(255, 200, 0)):
         self.y = y
         self.tile_width = tile_width
         self.height = height
+        self.speed = speed
         self.road_color = road_color
         self.dash_color = dash_color
+        self.offset = 0
     
     def draw_tile(self, surface, x):
         road_tile = pygame.Rect(x, self.y, self.tile_width, self.height)
@@ -22,7 +23,7 @@ class Road():
 
         dash_width = 50
         dash_height = 20
-        gap = 20
+        gap = 21
 
         dash_y = road_tile.centery - dash_height // 2
         dash_x = x + 10
@@ -35,17 +36,16 @@ class Road():
     def draw(self, surface):
         screen_width = surface.get_width()
 
-        for x in range(-500, 0):
+        for x in range(self.offset, screen_width + self.tile_width, self.tile_width):
             self.draw_tile(surface, x)
 
-
-class Car():
-
-    def __init__(self):
-        pass
+    def scroll(self):
+        self.offset -= self.speed
+        if self.offset <= -self.tile_width:
+            self.offset = 0
         
         
-class City():
+class Skyline():
 
     def __init__(self):
         pass
@@ -72,6 +72,8 @@ def main():
     height = pygame.display.Info().current_h
     resolution = (width, height)
     screen = pygame.display.set_mode(resolution)
+    car_img = pygame.image.load("flying_car.png")
+    scaled_img = pygame.transform.scale(car_img, resolution)
     road = Road(y=resolution[1]-220, tile_width=500, height=220)
     running = True
     while running:
@@ -83,6 +85,8 @@ def main():
                     running = False
         screen.fill("black")
         road.draw(screen)
+        road.scroll()
+        screen.blit(scaled_img, (0, 0))
         pygame.display.flip()
         dt = clock.tick(30)
     pygame.quit()
